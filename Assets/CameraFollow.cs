135df87cf98a1753f5c2d5ad532616c9e29376db
deupsh <01psh0845@naver.com>
2024-11-17 19:09:59 +0900

    241117 수정
    241117 수정

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,21 +6,25 @@ public class CameraFollow : MonoBehaviour
 {
     [Header("Camera Settings")]
     public Transform target;  // 카메라가 따라갈 대상 (플레이어)
-    public Vector3 offset = new Vector3(0, 5, -10);  // 카메라와 플레이어 사이의 거리
-    public float smoothSpeed = 0.125f;  // 카메라 이동의 부드러움 정도
+    public Vector3 offset = new Vector3(0, 2, -5);  // 카메라와 플레이어 사이의 거리 (플레이어 시점에 맞게 조정)
+    
+    public float zoomLevel = 5f;  // 줌 레벨 (플레이어 중심 시점에서 적절한 크기로 보기 위한 값)
+
+    void Start()
+    {
+        // 초기 줌 설정
+        Camera.main.orthographicSize = zoomLevel; 
+    }
 
     void LateUpdate()
     {
         if (target != null)
         {
-            // 목표 위치 계산 (플레이어 위치 + 오프셋)
+            // Lerp 없이 즉시 플레이어를 따라감
             Vector3 desiredPosition = target.position + offset;
 
-            // 부드럽게 카메라 이동 (Lerp 사용)
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-
-            // 카메라 위치 업데이트
-            transform.position = smoothedPosition;
+            // 카메라 위치를 즉시 업데이트
+            transform.position = desiredPosition;
         }
     }
 }
--- Assets/InfiniteMapGenerator.cs
+++ Assets/InfiniteMapGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -8,208 +9,330 @@ public enum Biome : int
     Snow = 0, Cave = 1, Ocean = 2, Desert = 3, Forest = 4, Swamp = 5, Lava = 6, Grassland = 7, MAX
 }
 
-public class RandomMapGenerater : MonoBehaviour
+public class InfiniteMapGenerator : MonoBehaviour
 {
     [Header("타일맵 관련")]
-    [SerializeField] private GameObject tileMapPrefab;
-    [SerializeField] private TileBase snow, snow2, cave, cave2, ocean, ocean2, desert, desert2, forest, forest2, swamp, swamp2, lava, lava2, grassland, grassland2;
+    [SerializeField] private Tilemap tileMap;
 
+    [Space]
+    [Header("눈 지형")]
+    [SerializeField] private TileBase snow, snow2;
+    [Header("동굴 지형")]
+    [SerializeField] private TileBase cave, cave2;
+    [Header("바다 지형")]
+    [SerializeField] private TileBase ocean, ocean2;
+    [Header("사막 지형")]
+    [SerializeField] private TileBase desert, desert2;
+    [Header("숲 지형")]
+    [SerializeField] private TileBase forest, forest2;
+    [Header("습지 지형")]
+    [SerializeField] private TileBase swamp, swamp2;
+    [Header("용암 지형")]
+    [SerializeField] private TileBase lava, lava2;
+    [Header("초원 지형")]
+    [SerializeField] private TileBase grassland, grassland2;
+
+    [Space]
     [Header("값 관련")]
-    [SerializeField] private float mapScale = 0.01f;
-    [SerializeField] private int chunkSize = 4;
-    [SerializeField] private int viewDistance = 3;
+    [SerializeField] private float mapScale = 0.01f; // 노이즈 스케일
+    [SerializeField] private int chunkSize = 32; // 하나의 청크 크기
+    [SerializeField] private int octaves = 3; // 노이즈 옥타브 수
+    [SerializeField] private int pointNum = 3; // 랜덤 포인트 개수
 
-    private Dictionary<Vector2Int, GameObject> loadedChunks = new Dictionary<Vector2Int, GameObject>();
-    private float seed;
-    private int octaves = 3;
-    private int pointNum = 10;
+    private float seed; // 시드 값
+    private Vector3Int previousPlayerChunk; // 이전 플레이어 위치의 청크 좌표
+    private Dictionary<Vector3Int, bool> generatedChunks = new Dictionary<Vector3Int, bool>(); // 이미 생성된 청크 캐싱
+    // 타일 캐싱을 위한 딕셔너리 추가 (타일 재생성 방지)
+    private Dictionary<Vector3Int, TileBase> tileCache = new Dictionary<Vector3Int, TileBase>();
 
-    private void Start()
+   private void Start()
     {
-        seed = Random.Range(0f, 10000f);
-        UpdateChunks(Vector2Int.zero);
+        seed = Random.Range(0f, 10000f); // 시드 값 초기화
+        previousPlayerChunk = GetPlayerChunkPosition();
+        GenerateChunk(previousPlayerChunk); // 시작할 때 플레이어 주변 청크를 생성
+        StartCoroutine(UpdateChunks()); // 플레이어 이동에 따라 청크 업데이트
+
+        // 카메라 확대/축소 조정 (타일맵 해상도에 맞춤)
+        Camera.main.orthographicSize = (Screen.height / 2f) / tileMap.cellSize.y;
     }
 
-    private void Update()
+    // 플레이어의 현재 청크 좌표를 가져오는 함수
+    private Vector3Int GetPlayerChunkPosition()
     {
-        Vector2Int currentPlayerChunkPos = GetPlayerChunkPosition();
-        UpdateChunks(currentPlayerChunkPos);
+        Vector3 playerPosition = Camera.main.transform.position; // 카메라를 기준으로 플레이어 위치 추적
+        return new Vector3Int(Mathf.FloorToInt(playerPosition.x / chunkSize), Mathf.FloorToInt(playerPosition.y / chunkSize), 0);
     }
 
-    private Vector2Int GetPlayerChunkPosition()
+    // 플레이어 이동에 따라 청크를 업데이트하는 코루틴 함수
+    private IEnumerator UpdateChunks()
     {
-        Vector3 playerPosition = Camera.main.transform.position;
-        return new Vector2Int(Mathf.FloorToInt(playerPosition.x / chunkSize), Mathf.FloorToInt(playerPosition.y / chunkSize));
-    }
-
-    private void UpdateChunks(Vector2Int playerChunkPos)
-    {
-        HashSet<Vector2Int> activeChunks = new HashSet<Vector2Int>();
-
-        for (int xOffset = -viewDistance; xOffset <= viewDistance; xOffset++)
+        while (true)
         {
-            for (int yOffset = -viewDistance; yOffset <= viewDistance; yOffset++)
-            {
-                Vector2Int chunkCoord = new Vector2Int(playerChunkPos.x + xOffset, playerChunkPos.y + yOffset);
-                activeChunks.Add(chunkCoord);
+            Vector3Int currentPlayerChunk = GetPlayerChunkPosition();
 
-                if (!loadedChunks.ContainsKey(chunkCoord))
+            if (currentPlayerChunk != previousPlayerChunk)
+            {
+                GenerateSurroundingChunks(currentPlayerChunk);
+                previousPlayerChunk = currentPlayerChunk;
+            }
+
+            yield return new WaitForSeconds(0.5f); // 0.5초마다 체크
+        }
+    }
+
+    // 현재 플레이어 주변 청크들을 생성하는 함수
+    private void GenerateSurroundingChunks(Vector3Int centerChunk)
+    {
+        for (int xOffset = -1; xOffset <= 1; xOffset++)
+        {
+            for (int yOffset = -1; yOffset <= 1; yOffset++)
+            {
+                Vector3Int chunkPos = new Vector3Int(centerChunk.x + xOffset, centerChunk.y + yOffset, 0);
+
+                if (!generatedChunks.ContainsKey(chunkPos)) // 아직 생성되지 않은 청크만 생성
                 {
-                    // 비동기 함수 호출 시 StartCoroutine 사용
-                    StartCoroutine(CreateChunkAsync(chunkCoord.x * chunkSize, chunkCoord.y * chunkSize));
+                    GenerateChunk(chunkPos);
+                    generatedChunks[chunkPos] = true;
                 }
             }
         }
-
-        List<Vector2Int> chunksToUnload = new List<Vector2Int>();
-        foreach (var chunk in loadedChunks)
-        {
-            if (!activeChunks.Contains(chunk.Key))
-            {
-                chunksToUnload.Add(chunk.Key);
-            }
-        }
-
-        foreach (var chunkCoord in chunksToUnload)
-        {
-            UnloadChunk(chunkCoord);
-        }
     }
 
-    private IEnumerator CreateChunkAsync(int startX, int startY)
-    {
-        GameObject tileMapObj = Instantiate(tileMapPrefab);
-        tileMapObj.transform.position = new Vector3(startX, startY);
-        Tilemap tileMapInstance = tileMapObj.GetComponent<Tilemap>();
+    // 특정 청크를 생성하는 함수
+   private void GenerateChunk(Vector3Int chunkPos)
+   {
+       float[,] noiseArr = GenerateNoise(chunkPos);
+       Vector2[] randomPoints = GenerateRandomPos(pointNum);
+       Vector2[] biomePoints = GenerateRandomPos((int)Biome.MAX);
+       Biome[,] biomeArr = GenerateBiome(randomPoints, biomePoints);
 
-        for (int xOffset = 0; xOffset < chunkSize; xOffset++)
-        {
-            for (int yOffset = 0; yOffset < chunkSize; yOffset++)
-            {
-                int xPos = startX + xOffset;
-                int yPos = startY + yOffset;
-                Vector3Int tilePosition = new Vector3Int(xOffset, yOffset, 0);
-                tileMapInstance.SetTile(tilePosition, GetTileByHight(GenerateNoiseAt(xPos, yPos), GenerateBiomeAt(xPos, yPos)));
-            }
-            yield return null; // 한 프레임 대기
-        }
+       for (int x = 0; x < chunkSize; x++)
+       {
+           for (int y = 0; y < chunkSize; y++)
+           {
+               Vector3Int tilePos = new Vector3Int(chunkPos.x * chunkSize + x, chunkPos.y * chunkSize + y, 0);
 
-        loadedChunks[new Vector2Int(startX / chunkSize, startY / chunkSize)] = tileMapObj;
-    }
+               if (!tileCache.ContainsKey(tilePos)) // 이미 생성된 타일은 다시 생성하지 않음
+               {
+                   TileBase tile = GetTileByHight(noiseArr[x, y], biomeArr[x, y]);
+                   tileMap.SetTile(tilePos, tile);
+                   tileCache[tilePos] = tile; // 캐싱하여 재생성 방지
+               }
+           }
+       }
+   }
 
-    private void UnloadChunk(Vector2Int chunkCoord)
-    {
-        if (loadedChunks.ContainsKey(chunkCoord))
-        {
-            Destroy(loadedChunks[chunkCoord]);
-            loadedChunks.Remove(chunkCoord);
-        }
-    }
+   // 노이즈 배열을 생성하는 함수 (청크별로)
+   private float[,] GenerateNoise(Vector3Int chunkPos)
+   {
+       float[,] noiseArr=new float[chunkSize,chunkSize];
+       float min=float.MaxValue,max=float.MinValue;
 
-    private float GenerateNoiseAt(int x, int y)
-    {
-        float lacunarity = 2.0f;
-        float gain = 0.5f;
+       for(int x=0;x<chunkSize;x++)
+       {
+           for(int y=0;y<chunkSize;y++)
+           {
+               float lacunarity=2.0f;
+               float gain=0.5f;
 
-        float amplitude = 0.5f;
-        float frequency = 1f;
+               float amplitude=0.5f;
+               float frequency=1f;
 
-        float noiseValue = 0f;
+               int worldX=chunkPos.x*chunkSize+x;
+               int worldY=chunkPos.y*chunkSize+y;
 
-        for (int i = 0; i < octaves; i++)
-        {
-            noiseValue += amplitude * (Mathf.PerlinNoise(seed + (x * mapScale * frequency), seed + (y * mapScale * frequency)) * 2 - 1);
-            frequency *= lacunarity;
-            amplitude *= gain;
-        }
-        return Mathf.InverseLerp(-1f, 1f, noiseValue);
-    }
+               for(int i=0;i<octaves;i++)
+               {
+                   noiseArr[x,y]+=amplitude*(Mathf.PerlinNoise(
+                       seed+(worldX*mapScale*frequency),
+                       seed+(worldY*mapScale*frequency))*2-1);
 
-    private Biome GenerateBiomeAt(int x, int y)
-    {
-        Vector2[] randomPoints = GenerateRandomPos(pointNum);
-        Vector2[] biomePoints = GenerateRandomPos((int)Biome.MAX);
+                   frequency*=lacunarity;
+                   amplitude*=gain;
+               }
 
-        float minDist = float.MaxValue;
-        int closestPointIndex = -1;
+               if(noiseArr[x,y]<min)min=noiseArr[x,y];
+               if(noiseArr[x,y]>max)max=noiseArr[x,y];
+           }
+       }
 
-        for (int i = 0; i < randomPoints.Length; i++)
-        {
-            float dist = Vector2.Distance(randomPoints[i], new Vector2(x, y));
-            if (dist < minDist)
-            {
-                minDist = dist;
-                closestPointIndex = i;
-            }
-        }
-        return (Biome)GetMinIdx(randomPoints[closestPointIndex], biomePoints);
-    }
+       for(int x=0;x<chunkSize;x++)
+       {
+           for(int y=0;y<chunkSize;y++)
+           {
+               noiseArr[x,y]=Mathf.InverseLerp(min,max,noiseArr[x,y]);
+           }
+       }
 
-    private Vector2[] GenerateRandomPos(int num)
-    {
-        Vector2[] arr = new Vector2[num];
-        for (int i = 0; i < num; i++) arr[i] = new Vector2(Random.Range(0, chunkSize - 1), Random.Range(0, chunkSize - 1));
-        return arr;
-    }
+       return noiseArr;
+   }
 
-    private int GetMinIdx(Vector2 point, Vector2[] biomeArr)
-    {
-        int curIdx = 0; float minDist = float.MaxValue;
-        for (int i = 0; i < biomeArr.Length; i++)
-        {
-            float dist = Vector2.Distance(point, biomeArr[i]);
-            if (dist < minDist) { minDist = dist; curIdx = i; }
-        }
-        return curIdx;
-    }
+   // 바이옴 배열을 생성하는 함수
+   private Biome[,] GenerateBiome(Vector2[] points, Vector2[] biomePoints)
+   {
+       Biome[,] biomeArr=new Biome[chunkSize,chunkSize];
 
-    private TileBase GetTileByHight(float hight, Biome biome)
-    {
-        if (biome == Biome.Snow)
-        {
-            if (hight <= 0.35f) return snow;
-            else if (hight <= 0.45f) return snow2;
-            else return snow;
-        }
-        else if (biome == Biome.Cave)
-        {
-            if (hight <= 0.35f) return cave;
-            else if (hight <= 0.45f) return cave2;
-            else return cave;
-        }
-        else if (biome == Biome.Ocean)
-        {
-            if (hight <= 0.35f) return ocean;
-            else return ocean;
-        }
-        else if (biome == Biome.Desert)
-        {
-            if (hight <= 0.35f) return desert;
-            else return desert;
-        }
-        else if (biome == Biome.Forest)
-        {
-            if (hight <= 0.35f) return forest;
-            else if (hight <= 0.45f) return forest2;
-            else return forest;
-        }
-        else if (biome == Biome.Swamp)
-        {
-            if (hight <= 0.35f) return swamp;
-            else if (hight <= 0.45f) return swamp2;
-            else return swamp;
-        }
-        else if (biome == Biome.Lava)
-        {
-            if (hight <= 0.35f) return lava;
-            else if (hight <= 0.45f) return lava2;
-            else return lava;
-        }
-        else
-        {
-            if (hight <= 0.35f) return grassland;
-            else if (hight <= 0.45f) return grassland2;
-            else return grassland;
-        }
-    }
+       for(int x=0;x<chunkSize;x++)
+       {
+           for(int y=0;y<chunkSize;y++)
+           {
+               float minDist=float.MaxValue;
+               int idx=-1;
+
+               for(int i=0;i<pointNum;i++)
+               {
+                   float dist=Vector2.Distance(points[i],new Vector2(x,y));
+
+                   if(dist<minDist)
+                   {
+                       minDist=dist;
+                       idx=i;
+                   }
+               }
+
+               biomeArr[x,y]=(Biome)GetMinIdx(points[idx],biomePoints);
+           }
+       }
+
+       return biomeArr;
+   }
+
+   // 랜덤 위치 배열을 생성하는 함수
+   private Vector2[] GenerateRandomPos(int num)
+   {
+       Vector2[] arr=new Vector2[num];
+
+       for(int i=0;i<num;i++)
+       {
+           int x=Random.Range(0,chunkSize-1);
+           int y=Random.Range(0,chunkSize-1);
+
+           arr[i]=new Vector2(x,y);
+       }
+
+       return arr;
+   }
+
+   // 가장 가까운 바이옴 인덱스를 계산하는 함수
+   private int GetMinIdx(Vector2 point,Vector2[] biomeArr)
+   {
+       int curIdx=0;
+       float min=float.MaxValue;
+
+       for(int i=0;i<biomeArr.Length;i++)
+       {
+           float value=Vector2.Distance(point,biomeArr[i]);
+
+           if(min>value)
+           {
+               min=value;
+               curIdx=i;
+           }
+       }
+
+       return curIdx;
+   }
+
+   // 타일맵에 타일을 설정하는 함수
+   private void SettingTileMap(float[,] noiseArr,Biome[,] biomeArr)
+   {
+       Vector3Int point=Vector3Int.zero;
+
+       for(int x=0;x<chunkSize;x++)
+       {
+           for(int y=0;y<chunkSize;y++)
+           {
+               point.Set(x,y,0);
+               tileMap.SetTile(point,GetTileByHight(noiseArr[x,y],biomeArr[x,y]));
+           }
+       }
+   }
+
+   // 높이에 따른 타일을 결정하는 함수
+   private TileBase GetTileByHight(float hight,Biome biome)
+   {
+       if(biome==Biome.Snow)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return snow;
+               case<=0.45f:return snow2;
+               case<=0.6f:return snow;
+               default:return snow;
+           }
+       }
+       else if(biome==Biome.Cave)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return cave;
+               case<=0.45f:return cave2;
+               case<=0.6f:return cave;
+               default:return cave2;
+
+           }
+       }
+       else if(biome==Biome.Ocean)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return ocean;
+               case<=0.45f:return ocean2;
+               case<=0.6f:return ocean;
+               default:return ocean2;
+           }
+       }
+       else if(biome==Biome.Desert)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return desert;
+               case<=0.45f:return desert2;
+               case<=0.6f:return desert;
+               default:return desert2;
+           }
+       }
+       else if(biome==Biome.Forest)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return forest;
+               case<=0.45f:return forest2;
+               case<=0.6f:return forest;
+               default:return forest2;
+           }
+       }
+       else if(biome==Biome.Swamp)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return swamp;
+               case<=0.45f:return swamp2;
+               case<=0.6f:return swamp;
+               default:return swamp2;
+           }
+       }
+       else if(biome==Biome.Lava)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return lava;
+               case<=0.45f:return lava2;
+               case<=0.6f:return lava;
+               default:return lava2;
+           }
+       }
+       else if(biome==Biome.Grassland)
+       {
+           switch(hight)
+           {
+               case<=0.35f:return grassland;
+               case<=0.45f:return grassland2;
+               case<=0.6f:return grassland;
+               default:return grassland2;
+           }
+       }
+       
+      return grassland; 
+   } 
 }
